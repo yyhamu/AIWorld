7cf47f2f236b5a937464c747a41920393d014cd0
yyhamu <128976615+yyhamu@users.noreply.github.com>
2023-06-03 22:31:04 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>AI.Chat Web</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI Chat Role.
+          Welcome To AI.Chat World!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
