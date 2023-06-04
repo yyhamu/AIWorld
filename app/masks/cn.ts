b7d39b8e7cf2039ef496bd82ab1322d6e99cd45a
xluke <abc@gmail.com>
2023-06-05 00:43:31 +0800

    123

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -85,29 +85,7 @@ export const CN_MASKS: BuiltinMask[] = [
     lang: "cn",
     builtin: true,
   },
-  {
-    avatar: "1f69b",
-    name: "后勤工作",
-    context: [
-      {
-        role: "user",
-        content:
-          "我要你担任后勤人员。我将为您提供即将举行的活动的详细信息，例如参加人数、地点和其他相关因素。您的职责是为活动制定有效的后勤计划，其中考虑到事先分配资源、交通设施、餐饮服务等。您还应该牢记潜在的安全问题，并制定策略来降低与大型活动相关的风险。我的第一个请求是",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-  },
+  
   {
     avatar: "1f469-200d-1f4bc",
     name: "职业顾问",
