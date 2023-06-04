f42c44a2a345bac22a68ad67854a8fddf6d90638
xluke <abc@gmail.com>
2023-06-05 00:16:19 +0800

    1

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -25,8 +25,7 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     hideContext:true,
-    hideContext:true,
-    hideContext:true,
+
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
