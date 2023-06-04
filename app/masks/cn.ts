26bf7ef125a92770c28eb4279505824d338b1478
xluke <abc@gmail.com>
2023-06-05 00:53:11 +0800

    123

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -132,29 +132,7 @@ export const CN_MASKS: BuiltinMask[] = [
     lang: "cn",
     builtin: true,
   },
-  {
-    avatar: "1f4da",
-    name: "语言检测器",
-    context: [
-      {
-        role: "user",
-        content:
-          "我希望你充当语言检测器。我会用任何语言输入一个句子，你会回答我，我写的句子在你是用哪种语言写的。不要写任何解释或其他文字，只需回复语言名称即可。我的第一句话是：",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-  },
+  
   {
     avatar: "1f4d5",
     name: "小红书写手",
