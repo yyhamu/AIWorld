2bc70ae6cd7e4501730f065fe294a6be8fb36960
yyhamu <luming@gmail.com>
2023-06-06 15:10:02 +0800

    qqq

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-    "请[点击这里/点击这里](/#/settings)页输入正确的访问密码",
+    "请[点击这里输入访问密码](/#/settings)",
     //"访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。",
   },
   ChatItem: {
